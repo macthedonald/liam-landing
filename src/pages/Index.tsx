b03880e187e54f1f7fdeabb07a4071fd059585c4
macthedonald <ogamacd@gmail.com>
2025-07-11 05:35:08 +0100

    second commit

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,6 @@
 
 import React, { useEffect } from "react";
+import { Analytics } from "@vercel/analytics/next"
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import HumanoidSection from "@/components/HumanoidSection";
